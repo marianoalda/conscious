ea6f62c19c13141ff4f8545f123629fd8bd1fa5c
Mariano Jesús Alda Membrives <mariano.alda@gmail.com>
2026-09-17 00:29:05 +0200

    Standard arguments processing (template, not actual parameters).

--- modules/conscious/src/main.c
+++ modules/conscious/src/main.c
@@ -1,6 +1,83 @@
+#include <getopt.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(int argc, char *argv[]) {
-    printf("Conscious Project - Main and Sync module.\n");
+typedef struct {
+    int verbose;
+    const char *config_file;
+    const char *name;
+} options_t;
+
+static void print_help(const char *program)
+{
+    printf("Usage: %s [OPTIONS]\n\n", program);
+    printf("Options:\n");
+    printf("  -h, --help              Show this help\n");
+    printf("  -v, --verbose           Enable verbose output\n");
+    printf("  -c, --config FILE       Configuration file\n");
+    printf("  -n, --name NAME         Node/process name\n");
+}
+
+static int parse_arguments(int argc, char **argv, options_t *options)
+{
+    static const struct option long_options[] = {
+        {"help",    no_argument,       NULL, 'h'},
+        {"verbose", no_argument,       NULL, 'v'},
+        {"config",  required_argument, NULL, 'c'},
+        {"name",    required_argument, NULL, 'n'},
+        {NULL,      0,                 NULL,  0}
+    };
+
+    int option;
+
+    while ((option = getopt_long(argc, argv, "hvc:n:", long_options, NULL)) != -1) {
+        switch (option) {
+        case 'h':
+            print_help(argv[0]);
+            exit(EXIT_SUCCESS);
+
+        case 'v':
+            options->verbose = 1;
+            break;
+
+        case 'c':
+            options->config_file = optarg;
+            break;
+
+        case 'n':
+            options->name = optarg;
+            break;
+
+        default:
+            return -1;
+        }
+    }
+
     return 0;
 }
+
+int main(int argc, char **argv)
+{
+    options_t options = {
+        .verbose = 0,
+        .config_file = NULL,
+        .name = NULL
+    };
+
+    if (parse_arguments(argc, argv, &options) != 0) {
+        fprintf(stderr, "Try '%s --help' for more information.\n", argv[0]);
+        return EXIT_FAILURE;
+    }
+
+    /* initialization */
+    printf("Conscious Project - Main and Sync module.\n");
+
+    /* Main logic here */
+
+    /* Exit */
+    printf("Exiting.\n");
+
+    return EXIT_SUCCESS;
+}
+
+ 
